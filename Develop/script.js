f20629a83da10a00d95770a264f072020b233a2d
TonnTribe <127579030+TonnTribe@users.noreply.github.com>
2023-04-02 22:24:33 -0700

    adding to script.js file

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  var passwordLength = prompt("8-128 characters");
+
+  var numbers = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"];
+  function generateString(length) {
+    let result = ' ';
+    const numbersLength = numbers.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += numbers.charAt(Math.floor(Math.random() * numbersLength));
+    }
+
+    return result;
+}
+
+console.log(generateString(passwordLength));
 
 }
 
